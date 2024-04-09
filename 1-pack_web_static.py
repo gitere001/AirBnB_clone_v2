d5e4d6efe27679deaa19eca819de80e3649e4710
James Gitere <sasmuypwa2019@gmail.com>
2024-04-09 10:23:57 -0700

    updated

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,30 +1,21 @@
-from fabric import task
+#!/usr/bin/python3
+'''Fabric script to generate .tgz archive'''
+
+from fabric.api import local
 from datetime import datetime
-import os
+
+from fabric.decorators import runs_once
 
 
-@task
-def do_pack(c):
-    """Generate a .tgz archive from the contents of the web_static folder."""
-    # Define the current date and time
-    now = datetime.now()
-    date_time = now.strftime("%Y%m%d%H%M%S")
+@runs_once
+def do_pack():
+    '''generates .tgz archive from the contents of the web_static folder'''
+    local("mkdir -p versions")
+    path = ("versions/web_static_{}.tgz"
+            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+    result = local("tar -cvzf {} web_static"
+                   .format(path))
 
-    # Name of the archive
-    archive_name = "web_static_{}.tgz".format(date_time)
-
-    # Path of the archive
-    archive_path = "versions/{}".format(archive_name)
-
-    # Create the versions folder if it doesn't exist
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
-
-    # Compress the contents of the web_static folder into the archive
-    result = c.local("tar -cvzf {} web_static".format(archive_path))
-
-    # Check if the compression was successful
     if result.failed:
         return None
-    else:
-        return archive_path
+    return path
