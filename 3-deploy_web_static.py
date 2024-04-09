5b43413654079938c2d48af2f8af76b79bd05288
James gitere <sasmuypwa2019.com>
2024-04-09 20:35:38 +0300

    pycodestyle

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -10,6 +10,7 @@ from fabric.api import run
 # Define the list of remote servers to deploy to
 env.hosts = ["34.207.221.247", "100.26.152.41"]
 
+
 def do_pack():
     """
     Create a tar gzipped archive of the directory web_static.
@@ -17,9 +18,10 @@ def do_pack():
     This function creates a compressed archive of the web_static directory
     containing static website files. The archive is named with a timestamp
     to ensure uniqueness.
-    
+
     Returns:
-        str: The path of the created archive, or None if the archive creation failed.
+        str: The path of the created archive,
+        or None if the archive creation failed.
     """
     dt = datetime.utcnow()
     file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
@@ -36,6 +38,7 @@ def do_pack():
     if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
     return file
+
 
 def do_deploy(archive_path):
     """
@@ -59,10 +62,12 @@ def do_deploy(archive_path):
     if run("rm -rf /data/web_static/releases/{}/".format(name)).failed is True:
         return False
     # Create a new directory for the deployment
-    if run("mkdir -p /data/web_static/releases/{}/".format(name)).failed is True:
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
     # Extract the archive into the deployment directory
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name)).failed is True:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
         return False
     # Remove the temporary archive file
     if run("rm /tmp/{}".format(file)).failed is True:
@@ -72,22 +77,28 @@ def do_deploy(archive_path):
            "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
     # Remove the now-empty directory created by the extraction
-    if run("rm -rf /data/web_static/releases/{}/web_static".format(name)).failed is True:
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
         return False
     # Remove the current symbolic link
     if run("rm -rf /data/web_static/current").failed is True:
         return False
     # Create a new symbolic link to the deployment directory
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name)).failed is True:
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
         return False
     return True
 
+
 def deploy():
     """
-    Create a tar gzipped archive of the web_static directory and deploy it to remote servers.
+    Create a tar gzipped archive of the web_static directory
+    and deploy it to remote servers.
 
-    This function orchestrates the deployment process by creating an archive of the
-    web_static directory, distributing it to the specified web servers, and deploying
+    This function orchestrates the deployment process by creating an
+    archive of the
+    web_static directory, distributing it to the specified web
+    servers, and deploying
     the contents to serve static files.
 
     Returns:
