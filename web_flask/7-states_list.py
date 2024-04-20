3372905dd19e8f5431db19378aaee254d617e5f5
James gitere <sasmuypwa2019.com>
2024-04-20 15:21:00 +0300

    make executable

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""Starts a flask app
-    listens to 0.0.0.0:5000
-
 """
+Starts a Flask app
+Listens to 0.0.0.0:5000
+"""
+
 from models import storage
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
+    """
+    Displays an HTML page with a list of all State objects in DBStorage.
     States are sorted by name.
     """
     states = storage.all("State")
@@ -21,7 +22,9 @@ def states_list():
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+    """
+    Remove the current SQLAlchemy session.
+    """
     storage.close()
 
 
